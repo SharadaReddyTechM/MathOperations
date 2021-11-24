e883a3d401b96e8077f3f6a075179441e7920798
admin2311 <admin2311@admin2311>
2021-11-24 20:03:16 +0530

    Add project files.

---
+++ MathOperations/MathLogics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class MathLogics
+    {
+        public static int Square(int num)
+        {
+            return (num * num);
+        }
+
+        internal static int Cube(int num)
+        {
+            //return Square(num) * num;
+            //return (int)Math.Pow(num, 3);
+            return (num * num * num);
+        }
+
+        public static int Reverse(int num)
+        {
+            int rev = 0;
+
+            while (num!=0)
+            {
+                rev = rev * 10 + num % 10;
+                num /= 10;
+            }
+
+            return rev;
+        }
+        public static bool Pallindrome(int num)
+        {
+            bool isPallindrome = false;
+
+            int rev = Reverse(num);
+
+            return isPallindrome=(rev==num)?true:false;
+        }
+    }
+}
